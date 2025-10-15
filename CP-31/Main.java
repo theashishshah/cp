d862cce6f1e1f729da84b8e661cb9073e57364ea
Ashish Shah <eiepixxstuff@gmail.com>
2025-10-15 16:01:08 +0530

    Day-9

--- CP-31/Main.java
+++ CP-31/Main.java
@@ -7,9 +7,14 @@ public class Main {
         // Your code here
         int t = sc.nextInt();
         while(t-- > 0){
-
+            int n = sc.nextInt(); // Number of teams
+            int[] nums = new int[n - 1];
+            for(int i = 0; i < n - 1; i++) nums[i] = sc.nextInt();
+            int ans = 0;
+            for(int num: nums) ans += num;
+            ans = - ans;
+            System.out.println(ans);
         }
-
         sc.close();
     }
 }
